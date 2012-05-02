5f07cd26df3f82ae79a144666ddfed6c1b8483e8
Rune Holmgren <Raane.Holm@gmail.com>
2012-05-02 17:32:21 +0200

    fixed the levelpharse, so i creates binary maps

--- levelpharser.py
+++ levelpharser.py
@@ -4,10 +4,10 @@ output = ""
 for i in range(12):    
     for letter in stdin.readline():
         if not letter == chr(10) and not letter == ' ':
-            output = output + chr(ord(letter))
+            output = output + chr(ord(letter) - 48)
     #print ""
 #while(currentline = stdin.readline()):
 for line in stdin.readlines():
     for coor in line.split(','):
-        output = output + chr(int(coor)+48)
+        output = output + chr(int(coor))
 print output,
